42d0c1195c4e838de60c477d8f8ef5d277594a84
javriq <riquelmejavierariel@gmail.com>
2024-08-03 13:16:40 -0300

    Adding handlerbars for products

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 const express = require('express');
+const { Server } = require('socket.io');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const ProductManager = require('./managers/ProductManager');
+
 const app = express();
-const productsRouter = require('./routes/products');
-const cartsRouter = require('./routes/carts');
+const http = require('http').createServer(app);
+const io = new Server(http);
+const productManager = new ProductManager();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
+app.get('/products', async (req, res) => {
+    const products = await productManager.getAll();
+    res.render('index', { products });
+});
 
-const PORT = 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/realtimeproducts', async (req, res) => {
+    const products = await productManager.getAll();
+    res.render('realTimeProducts', { products });
+});
+
+io.on('connection', async (socket) => {
+    console.log('cliente conectado');
+    const products = await productManager.getAll();
+    socket.emit('updateProducts', products);
+
+    socket.on('newProduct', async (product) => {
+        const newProduct = await productManager.addProduct(product);
+        const updatedProducts = await productManager.getAll();
+        io.emit('updateProducts', updatedProducts);
+    });
+
+    socket.on('deleteProduct', async (productId) => {
+        await productManager.deleteProduct(parseInt(productId));
+        const updatedProducts = await productManager.getAll();
+        io.emit('updateProducts', updatedProducts);
+    });
+});
+
+http.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });
--- managers/ProductManager.js
+++ managers/ProductManager.js
@@ -1,43 +1,41 @@
-const fs = require('fs');
-const path = './data/products.json';
+const fs = require('fs').promises;
+const path = require('path');
+const crypto = require('crypto');
 
 class ProductManager {
     constructor() {
-        this.path = path;
-        if (!fs.existsSync(this.path)) {
-            fs.writeFileSync(this.path, JSON.stringify([]));
-        }
+        this.filePath = path.join(__dirname, '..', 'data', 'products.json');
     }
 
     async getAll() {
-        const data = await fs.promises.readFile(this.path, 'utf-8');
-        return JSON.parse(data);
+        const data = await fs.readFile(this.filePath, 'utf-8');
+        return JSON.parse(data || '[]');
     }
 
-    async getById(id) {
-        const products = await this.getAll();
-        return products.find(product => product.id === id);
+    generateId(products) {
+        return products.length ? products[products.length - 1].id + 1 : 1;
+    }
+
+    generateCode() {
+        return crypto.randomBytes(4).toString('hex');
     }
 
     async addProduct(product) {
         const products = await this.getAll();
-        product.id = products.length ? products[products.length - 1].id + 1 : 1;
-        products.push(product);
-        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
-        return product;
-    }
-
-    async updateProduct(id, updatedProduct) {
-        let products = await this.getAll();
-        products = products.map(product => (product.id === id ? { ...product, ...updatedProduct, id: product.id } : product));
-        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
-        return products.find(product => product.id === id);
+        const newProduct = { 
+            id: this.generateId(products), 
+            code: this.generateCode(), 
+            ...product 
+        };
+        products.push(newProduct);
+        await fs.writeFile(this.filePath, JSON.stringify(products, null, 2));
+        return newProduct;
     }
 
     async deleteProduct(id) {
         let products = await this.getAll();
         products = products.filter(product => product.id !== id);
-        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+        await fs.writeFile(this.filePath, JSON.stringify(products, null, 2));
     }
 }
 
